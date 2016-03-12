547e95df9a909dc2c8ec10621acc1f85d7f18ab9
Yami <yamilethmedina1@gmail.com>
2016-03-12 16:03:57 -0500

    working on adding users

--- service.php
+++ service.php
@@ -39,16 +39,24 @@ class OyeSocio extends Service
 	public function _registrarse(Request $request)
 	{
 		// get the name
-		$fullname = explode(" ", $request->query)
-		$responseContent = array(
-			"nombre" => $fullname[0],
-			"apellido" => $fullname[1]
-		);
+		$fullname = explode(" ", $request->query);
+		$firstName = $fullname[0];
+		$lastName = $fullname[1];
+
+		// save using the API
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL,"http://45.79.199.31:2016/OyeSocio/api/users"); // which URL you'll go to
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "firstName={$firstName}&lastName={$lastName}&email={$email}"); // parameters that will append to url
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$server_output = curl_exec ($ch);
+		curl_close ($ch);
+		// print_r($server_output);
 
 		// create the response
 		$response = new Response();
 		$response->setResponseSubject("Gracias para registrarse!");
-		$response->createFromTemplate("basic.tpl", $responseContent);
+		$response->createFromText("El resgisto se ha realizado satisfactoriamente.");
 		return $response;
 	}
 }
